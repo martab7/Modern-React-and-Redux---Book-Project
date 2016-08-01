999d19d002cfa221c7fcb6392c63cd1b737cd724
Anthony <martab7.am@gmail.com>
2016-08-01 16:05:35 -0500

    Creates new repository for all files corresponding to this book project using redux

---
+++ src/reducers/reducer_books.js
@@ -0,0 +1,8 @@
+export default function() {
+  return [
+    { title: 'Javascript: The Good Parts', pages: 400 },
+    { title: 'Harry Potter', pages: 390 },
+    { title: 'The Dark Tower', pages: 400 },
+    { title: 'The River God', pages: 700 }
+  ]
+}
